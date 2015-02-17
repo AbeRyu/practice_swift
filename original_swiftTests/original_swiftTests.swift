d9d43e13578002dd81f89a5cfe9564362056df94
Ryunosuke Abe <r.abe.dragon@gmail.com>
2015-02-17 14:58:54 +0900

    Initial Commit

---
+++ original_swiftTests/original_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  original_swiftTests.swift
+//  original_swiftTests
+//
+//  Created by 阿部 竜之介 on 2015/02/17.
+//  Copyright (c) 2015年 RyunosukeAbe. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class original_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
