3a781c26f730c25e9ddb99b754663956eb14c916
Ryunosuke Abe <r.abe.dragon@gmail.com>
2015-02-19 17:31:58 +0900

    ちょっとスッキリ

--- original_swift/ViewController.swift
+++ original_swift/ViewController.swift
@@ -109,61 +109,55 @@ class ViewController: UIViewController {
         // 生成したサイズを使って、ビューを生成
         
         //ポーズ
-        
-        var count1 : Int = 0
-        var count2 : Int = 0
         for(var i = 0.0 ; i < 1 ; i = i + 0.01){
             
             for(var j = -1.0 ; j < 1.5 ; j = j + 0.01){
                 
                 if(pow(i,2)+pow((j - pow(i,2/3)),2) <= 1 && pow(i,2)+pow((j - pow(i,2/3)),2) >= 0.95){
                     //ハート右
-                    var view_count1 = UIView(frame: rect)
-                    view_count1.layer.cornerRadius = 5
+                    var view = UIView(frame: rect)
+                    view.layer.cornerRadius = 5
                     
                     var boom1_x = Float(stPoint_x) + Float(i*100)
                     var boom1_y = Float(stPoint_y) - Float(j*90)
                     
                     //サイズと位置を指定します(x座標, y座標, width, height)
-                    view_count1.frame = CGRectMake(CGFloat(boom1_x) ,CGFloat(boom1_y) , 10, 10)
+                    view.frame = CGRectMake(CGFloat(boom1_x) ,CGFloat(boom1_y) , 10, 10)
                     
                     /* まとめて書く事も可能です */
                     //let view= UIView(frame:CGRectMake(0, 0, 150, 150))
                     
                     //viewの背景色（alphaは透過度）
-                    view_count1.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+                    view.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
                     
                     //view1をself.viewに追加
-                    self.view.addSubview(view_count1)
+                    self.view.addSubview(view)
                     
-                    count1 += 1
                     
                     //ハート右
-                    var view_count2 = UIView(frame: rect)
-                    view_count2.layer.cornerRadius = 5
+                    var view2 = UIView(frame: rect)
+                    view2.layer.cornerRadius = 5
                     
                     var boom2_x = Float(stPoint_x) - Float(i*100)
                     var boom2_y = Float(stPoint_y) - Float(j*90)
                     
                     //サイズと位置を指定します(x座標, y座標, width, height)
-                    view_count2.frame = CGRectMake(CGFloat(boom2_x) ,CGFloat(boom2_y) , 10, 10)
+                    view2.frame = CGRectMake(CGFloat(boom2_x) ,CGFloat(boom2_y) , 10, 10)
                     
                     /* まとめて書く事も可能です */
                     //let view= UIView(frame:CGRectMake(0, 0, 150, 150))
                     
                     //viewの背景色（alphaは透過度）
-                    view_count2.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+                    view2.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
                     
                     //view1をself.viewに追加
-                    self.view.addSubview(view_count2)
+                    self.view.addSubview(view2)
                     
-                    count2 += 1
                     
                 }
             }
             
         }
-        println(count1)
         
     }
     
